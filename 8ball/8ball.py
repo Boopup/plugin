085f7c1a56a2d35edfb8dab1c0410a2daf662175
Boopup <99917823+Boopup@users.noreply.github.com>
2023-02-25 13:59:23 -0600

    Update 8ball.py

--- 8ball/8ball.py
+++ 8ball/8ball.py
@@ -6,12 +6,10 @@ import magic8ball
 
 class EightBall(commands.Cog):
     """
-    Ask CCU a question and get an answer from a ever-growing list of answers.
+    Ask a Question, and CCU will answer it!
 
     Disclaimer: These answers are jokes and should be taken as jokes.
-    For legal advice, talk to a lawyer.
-    For general advice, don't take it from a bot.
-    """
+    For legal advice, talk to a lawyer. For general advice, don't take it from a bot.    """
 
     def __init__(self, bot):
         self.bot = bot
@@ -19,16 +17,14 @@ class EightBall(commands.Cog):
     @commands.command(aliases=["ateball", "8bl"])
     async def eightball(self, ctx, *, question):
         """
-        Ask Modmail 8-Ball a question and get a response!
+        Ask a question and get a response!
 
         Usage: [prefix]eightball <question>
         """
         embed = discord.Embed(
-            title=f"Question by {ctx.author}:", description=question, color=0x51EAFF
+            title=f"Question by {ctx.author}:", description=question, color=5614335
         )
-        embed.set_author(
-            name="8-Ball",
-        )
+
         embed.add_field(
             name="Answer:", value=random.choice(magic8ball.list), inline=False
         )
